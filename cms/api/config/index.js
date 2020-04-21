21cb5ba0ec6c3cdfd42f954091ceb772327107f4
Rodrigo <rodrigoeddie@gmail.com>
2020-04-21 14:47:25 -0300

    vue init

--- cms/api/config/index.js
+++ cms/api/config/index.js
@@ -2,6 +2,6 @@ module.exports = {
     ENV        : process.env.NODE || 'development',
     PORT       : process.env.PORT || 8080,
     JWT_SECRET : process.env.JWT_SECRET  || 'supersecretkey',
-    URL        : process.env.BASE_URL || 'http: //localhost: 8080',
+    URL        : process.env.BASE_URL || 'http: //localhost: 3000',
     MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://rodrigoeddie:5-7ae9FBEVKfb2q@cluster0-bduo3.mongodb.net/test?retryWrites=true&w=majority'
 }
