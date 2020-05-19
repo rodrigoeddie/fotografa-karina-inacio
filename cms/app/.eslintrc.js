19b6ffbe3bef7242e6a0135591453dcfb344b629
Rodrigo <rodrigoeddie@gmail.com>
2020-05-18 22:56:41 -0300

    Working...

--- cms/app/.eslintrc.js
+++ cms/app/.eslintrc.js
@@ -11,6 +11,10 @@ module.exports = {
     parser: 'babel-eslint',
   },
   rules: {
+    "linebreak-style": 0,
+    "global-require": 0,
+    "eslint linebreak-style": [0, "error", "windows"],
+    'max-len': ["error", { "code": 150 }],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
--- cms/app/src/components/HelloWorld.vue
+++ cms/app/src/components/HelloWorld.vue
@@ -1,32 +1,39 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+  <v-app id="inspire">
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center" >
+          <v-col cols="12" sm="8" md="4" >
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat >
+                <v-toolbar-title>Login form</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+
+              <v-card-text>
+                <v-form>
+                  <v-text-field label="Login" name="login" prepend-icon="person" type="text" ></v-text-field>
+                  <v-text-field id="password" label="Password" name="password" prepend-icon="lock" type="password" ></v-text-field>
+                </v-form>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String,
+    source: String,
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
