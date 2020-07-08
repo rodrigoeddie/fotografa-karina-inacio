fa03602907924d9522b568d9916cad94a394088d
Rodrigo de Melo <rodrigoeddie@gmail.com>
2020-07-07 21:30:53 -0300

    working

---
+++ cms/app/components/Sidebar.vue
@@ -0,0 +1,12 @@
+
+<template>
+    <aside class="menu">
+        <p class="menu-label">
+        Menu
+        </p>
+        <ul class="menu-list">
+        <li><nuxt-link to="/">Dashboard</nuxt-link></li>
+        <li><nuxt-link to="/about">Sobre</nuxt-link></li>
+        </ul>
+    </aside>
+</template>
---
+++ cms/app/pages/dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div>
+      <sidebar />
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from '~/components/Sidebar.vue'
+
+export default {
+  components: {
+    Sidebar
+  }
+}
+</script>
+
+<style>
+</style>
